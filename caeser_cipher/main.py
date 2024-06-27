c903d07ceda4ae5ea73ab02280383f158544bf64
damzymichael <105460672+damzymichael@users.noreply.github.com>
2024-06-27 01:49:41 +0100

    caeser cipher complete

--- caeser_cipher/main.py
+++ caeser_cipher/main.py
@@ -8,31 +8,23 @@ alphabet = [
 
 print(logo)
 
-def encrypt(text, shift):
-  encrypted = []
-  for ch in text:
-    if ch not in alphabet:
-      new_char = ch
+def caeser_cipher(start_text, shift_by, cipher_direction):
+  result_text = ''
+  if cipher_direction == 'decode':
+    shift_by *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_by
+      #Replacement for adding another set of alphabets to the list
+      if new_position > len(alphabet):
+        available_to_shift = len(alphabet) - 1 - position
+        new_position = shift_by - available_to_shift - 1
+      result_text += alphabet[new_position]
     else:
-      id = alphabet.index(ch)
-      shift_by = id + shift
-      new_char = alphabet[shift_by]
-    encrypted.append(new_char)
-  print(''.join(encrypted))
+      result_text += char
+  print(result_text)
 
-def decrypt(text, shift):
-  decrypted = ''
-  for ch in text:
-    if ch not in alphabet:
-      new_char = ch
-    else:
-      id = alphabet.index(ch)
-      shift_by = id - shift
-      new_char = alphabet[shift_by]  
-    decrypted += new_char
-  print(decrypted)
-
-# def caeser()
 
 should_continue = True
 
@@ -45,18 +37,10 @@ while should_continue:
 
   shift = shift % 26
 
-  if direction == 'encode':
-    encrypt(text, shift)
-  else:
-    decrypt(text, shift)
+  caeser_cipher(start_text=text, shift_by=shift, cipher_direction=direction)
 
   user_continue_preference = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
   if user_continue_preference == 'no':
     should_continue = False
     print('Goodbye!')
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
-# TODO-5: Refactor into one function 
